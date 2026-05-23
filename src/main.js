3e314d0f7468874cfd79d944a3c875cc9b0045f5
Jiří Náhlovský <nahlovsky.jiri@feg.eu>
2026-05-23 02:37:20 +0200

    fix: change router history mode from createWebHistory to createWebHashHistory

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ui from '@nuxt/ui/vue-plugin'
 import App from './App.vue'
 import './assets/main.css'
@@ -7,7 +7,7 @@ import './assets/main.css'
 const app = createApp(App)
 const router = createRouter({
   routes: [],
-  history: createWebHistory()
+  history: createWebHashHistory()
 })
 
 app.use(router)
